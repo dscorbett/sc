f0750e8340a3de76c7cc76029752150c89bf2d30
David Corbett <>
2009-07-24 14:02:20 -0400

    based on sc2.1.pl, with: the basis of intercategorialization comments

--- sc.pl
+++ sc.pl
@@ -1,35 +1,33 @@
 #!/bin/perl
-# based on sc2.0.pl, with:
-# simpler word boundaries
-# no backreferencing
+# based on sc2.1.pl, with:
+# the basis of intercategorialization
+# comments
 use feature "say";
 use strict;
 use warnings;
 
 open RULES, "<rules.txt" or die "rules.txt not found";
 my %cats;
+my @rulesAv;
+my @rulesAp;
+my $total;
 while (<RULES>) {
+  next if (/^\s*($|#)/); # comment or blank line
+  chomp;
+  ($_) = split /#/, $_;
   if (/ = /) {
-    chomp;
-    (my $cat, my $contents) = split / = /, $_, 4;
+    (my $cat, my $contents) = split / = /, $_, 2;
     die "Illegal character in category name: \"$1\"\n" if ($cat =~ /(\s)/);
     die "Illegal character in $cat: \"$1\"\n" if ($contents =~ /([(|)])/);
     die "Empty category: $cat\n" if ($contents =~ /^$/);
-    $cats{$cat} = "(" . join '|', split / +/, $contents . ")";
-  }
-}
-close RULES;
-
-open RULES, "<rules.txt" or die "rules.txt lost";
-my @rulesAv;
-my @rulesAp;
-while (<RULES>) {
-  chomp;
-  if (/ > /) {
+    my $tmp = [];
+    push @$tmp, split / +/, $contents;
+    $cats{$cat} = $tmp;
+  } elsif (/ > /) {
     my @rule = split / > /;
     my @avant = split (/\s+/, $rule[0]);
     my @apres = split (/\s+/, $rule[1]);
-    (my $avant, my $total) = parseAvant (@avant);
+    (my $avant, $total) = parseAvant (@avant);
     my $apres = parseApres ($total, @apres);
     push @rulesAv, $avant;
     push @rulesAp, $apres;
@@ -46,7 +44,7 @@ while (<WORDS>) {
     my $av = $rulesAv[$i];
     my $ap = $rulesAp[$i];
     @index = regindex ($word, $av, $index[$#index]);
-    if (1) {                                   #TODO: if the program should evaluate immediately
+    unless (0) {                             #TODO: unless the program should wait
       eval "\$word =~ s/^(.{$_})$av/\$1$ap/" foreach (@index);
       print " > $word";
      @index = (0);
@@ -61,7 +59,7 @@ sub parseAvant {
   foreach (@_) {
     $counter++;
     if (/^<([^>]+)>$/) {
-      exists $cats{$1} ? $avant .= $cats{$1} : die "Uninitialized category: $1\n";
+      exists $cats{$1} ? $avant .= parseCat ($1) : die "Uninitialized category: <$1>\n";
     } elsif (/^\$0*(\d+)$/) {
       $1 ne "0" && $1 < $counter ? $avant .= "(\\$1)" : die "Invalid backreference: \$$1\n";
     } elsif (/^#$/) {
@@ -77,8 +75,12 @@ sub parseApres {
   my $total = shift;
   my $apres;
   foreach (@_) {
-    if (/^<([^>]+)>$/) {
-      die "Use of category in apres: $1\n";
+    if (/^<([^>]+)>/) { # was /^<([^>]+)>$/
+      #die "Use of category in apres: $1\n";
+      die "Uninitialized category: <$1>\n" unless (exists $cats{$1});
+      die "Unspecified backreference for category <$1>: " unless (/^<$1>\$0*(\d+)$/);
+      die "Invalid backreference: \$$1\n" unless ($1 ne "0" && $1 == $total);
+      
     } elsif (/^\$0*(\d+)$/) {
       $1 ne "0" && $1 <= $total ? $apres .= "\$" . $1 : die "Invalid backreference: \$$1\n";
     } else {
@@ -86,6 +88,14 @@ sub parseApres {
     }
   }
   return $apres;
+}
+
+sub parseCat {
+  my @fish;
+  foreach (0 .. $#{$cats{vowel}}) {
+    push @fish, @{$cats{vowel}}[$_];
+  }
+  return "(" . join ("|", @fish) . ")";
 }
 
 sub regindex {
